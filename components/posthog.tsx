a1fe867a6a0973f4a5010b2950956bb85ae95c7a
levelupnation444-cell <levelupnation444@gmail.com>
2026-05-13 15:44:06 -0700

    Use posthog-js dependency for analytics init

---
+++ components/posthog.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import posthog from "posthog-js";
+
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com";
+
+export function PostHog() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (!POSTHOG_KEY) return;
+
+    posthog.init(POSTHOG_KEY, {
+      api_host: POSTHOG_HOST,
+      capture_pageview: false,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!POSTHOG_KEY || !pathname) return;
+
+    const query = searchParams.toString();
+    const url = query ? `${pathname}?${query}` : pathname;
+
+    posthog.capture("$pageview", { $current_url: url });
+  }, [pathname, searchParams]);
+
+  return null;
+}
