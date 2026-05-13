c0a12b9c943b5de174710332bab4daf18094301b
levelupnation444-cell <levelupnation444@gmail.com>
2026-05-13 15:51:20 -0700

    Fix PostHog integration for Next 16 static build

---
+++ components/posthog.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+import posthog from "posthog-js";
+
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com";
+
+export function PostHog() {
+  useEffect(() => {
+    if (!POSTHOG_KEY) return;
+
+    posthog.init(POSTHOG_KEY, {
+      api_host: POSTHOG_HOST,
+      capture_pageview: "history_change",
+    });
+  }, []);
+
+  return null;
+}
